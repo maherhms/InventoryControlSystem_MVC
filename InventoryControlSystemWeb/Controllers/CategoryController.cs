2685e0c370140f01832faa7e2b67d1637931f802
maherhms <maher.3000@outlook.fr>
2026-04-21 12:11:34 +0200

    Implement category edit form

--- InventoryControlSystemWeb/Controllers/CategoryController.cs
+++ InventoryControlSystemWeb/Controllers/CategoryController.cs
@@ -32,5 +32,30 @@ namespace InventoryControlSystemWeb.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category entity)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(entity);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
     }
 }
