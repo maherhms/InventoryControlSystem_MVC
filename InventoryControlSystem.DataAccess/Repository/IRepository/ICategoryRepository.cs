0b316d7609f63a1622233f639b61fff4af71f48d
maherhms <maher.3000@outlook.fr>
2026-05-04 15:48:37 +0200

    Add asp areas and implement unitOfWork for multiple CRUD Ops

---
+++ InventoryControlSystem.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using InventoryControlSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryControlSystem.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+    }
+}
--- InventoryControlSystemWeb/Areas/Customer/Controllers/HomeController.cs
+++ InventoryControlSystemWeb/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@ using System.Diagnostics;
 using InventoryControlSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace InventoryControlSystemWeb.Controllers
+namespace InventoryControlSystemWeb.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
