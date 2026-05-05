e9e490f82fba530476eb33591adf9f6c98c2bed4
maherhms <maher.3000@outlook.fr>
2026-05-05 09:28:11 +0200

    Implement products models and repo

--- InventoryControlSystem.DataAccess/Migrations/20260505071544_AddProductToDb.Designer.cs
+++ InventoryControlSystem.DataAccess/Migrations/20260505071544_AddProductToDb.Designer.cs
@@ -3,6 +3,7 @@ using InventoryControlSystem.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventoryControlSystem.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260505071544_AddProductToDb")]
+    partial class AddProductToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- InventoryControlSystem.DataAccess/Repository/ProductRepository.cs
+++ InventoryControlSystem.DataAccess/Repository/ProductRepository.cs
@@ -1,28 +1,26 @@
 ﻿using InventoryControlSystem.DataAccess.Data;
 using InventoryControlSystem.DataAccess.Repository.IRepository;
+using InventoryControlSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InventoryControlSystem.DataAccess.Repository
 {
-    public class UnitOfWork : IUnitOfWork
+    public class ProductRepository : Repository<Product> , IProductRepository
     {
         private ApplicationDbContext _db;
-        public ICategoryRepository Category { get; private set; }
-        public IProductRepository Product { get; private set; }
-        public UnitOfWork(ApplicationDbContext db)
+        public ProductRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
-            Category = new CategoryRepository(_db);
-            Product = new ProductRepository(_db);
         }
 
-        public void Save()
+        public void Update(Product obj)
         {
-            _db.SaveChanges();
+            _db.Products.Update(obj);
         }
     }
 }
