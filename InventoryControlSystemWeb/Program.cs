222dfb12f84e208e72a4ae942be03306c373c7b1
maherhms <maher.3000@outlook.fr>
2026-07-07 12:04:46 +0200

    Add .net authentication and loginpartial layout to home page

--- InventoryControlSystemWeb/Program.cs
+++ InventoryControlSystemWeb/Program.cs
@@ -2,6 +2,8 @@ using InventoryControlSystem.DataAccess.Data;
 using InventoryControlSystem.DataAccess.Repository.IRepository;
 using InventoryControlSystem.DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
+using InventoryControlSystemWeb.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -26,7 +30,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
---
+++ InventoryControlSystemWeb/Views/Shared/_LoginPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventoryControlSystemWeb.Views.Shared
+{
+    public class _LoginPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
